d90cddd380088d0e24ab6bc7e98265b2160c956a
Brian Duff <brian@dufflin.com>
2023-08-04 13:37:51 -0700

    format

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use air::AirPlugin;
+use anyhow::Result;
 use bevy::prelude::*;
 use cavern::CavernPlugin;
 use debug::DebugPlugin;
 use gamedata::GameDataPlugin;
-use anyhow::Result;
 use lives::LivesPlugin;
 use score::ScorePlugin;
 use text::TextPlugin;
@@ -31,40 +31,51 @@ static DISPLAY_SCREEN_WIDTH_CH: f32 = 32.;
 static DISPLAY_SCREEN_HEIGHT_CH: f32 = 24.;
 static BORDER_WIDTH_PX: f32 = SCALE * PIX_PER_CHAR * BORDER_WIDTH_CHARS;
 pub static SCREEN_WIDTH_PX: f32 = SCALE * PIX_PER_CHAR * DISPLAY_SCREEN_WIDTH_CH;
-pub static WINDOW_WIDTH_PX : f32  = SCREEN_WIDTH_PX + (BORDER_WIDTH_PX * BORDER_MUL);
+pub static WINDOW_WIDTH_PX: f32 = SCREEN_WIDTH_PX + (BORDER_WIDTH_PX * BORDER_MUL);
 static SCREEN_HEIGHT_PX: f32 = SCALE * PIX_PER_CHAR * DISPLAY_SCREEN_HEIGHT_CH;
-static WINDOW_HEIGHT_PX : f32 = SCREEN_HEIGHT_PX + (BORDER_WIDTH_PX * BORDER_MUL);
+static WINDOW_HEIGHT_PX: f32 = SCREEN_HEIGHT_PX + (BORDER_WIDTH_PX * BORDER_MUL);
 static PIX_PER_CHAR: f32 = 8.;
 static BORDER_MUL: f32 = 2.;
 
-
 pub fn handle_errors(In(result): In<Result<()>>) {
-    if let Err(e) = result {
-        eprintln!("Error: {}", e);
-    }
+  if let Err(e) = result {
+    eprintln!("Error: {}", e);
+  }
 }
 
-fn main() -> Result<()>  {
-    let window = Window {
-        title: "Miner Willy".into(),
-        resolution: (WINDOW_WIDTH_PX, WINDOW_HEIGHT_PX).into(),
-        ..default()
-    };
+fn main() -> Result<()> {
+  let window = Window {
+    title: "Miner Willy".into(),
+    resolution: (WINDOW_WIDTH_PX, WINDOW_HEIGHT_PX).into(),
+    ..default()
+  };
 
-    App::new()
-        .add_plugins(DefaultPlugins.
-            set(ImagePlugin::default_nearest())
-            .set(WindowPlugin { primary_window: Some(window), ..default() })) // prevents blurry sprites
-        .add_plugins((DebugPlugin, GameDataPlugin, CavernPlugin, TextPlugin, ScorePlugin, AirPlugin, WillyPlugin, LivesPlugin))
-        .add_systems(PostStartup, setup)
-        .insert_resource(ClearColor(Color::rgb(0.0, 0.0, 0.0)))
-        .run();
+  App::new()
+    .add_plugins(
+      DefaultPlugins
+        .set(ImagePlugin::default_nearest())
+        .set(WindowPlugin {
+          primary_window: Some(window),
+          ..default()
+        }),
+    ) // prevents blurry sprites
+    .add_plugins((
+      DebugPlugin,
+      GameDataPlugin,
+      CavernPlugin,
+      TextPlugin,
+      ScorePlugin,
+      AirPlugin,
+      WillyPlugin,
+      LivesPlugin,
+    ))
+    .add_systems(PostStartup, setup)
+    .insert_resource(ClearColor(Color::rgb(0.0, 0.0, 0.0)))
+    .run();
 
-    Ok(())
+  Ok(())
 }
 
-fn setup(
-    mut commands: Commands,
-)  {
-    commands.spawn(Camera2dBundle::default());
+fn setup(mut commands: Commands) {
+  commands.spawn(Camera2dBundle::default());
 }
